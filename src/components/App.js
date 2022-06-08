95baa934bb19f99a8ec6887d31e55d1c7d754871
Amjad <amjadmaqsuma@gmail.com>
2022-06-08 18:34:24 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Toggle from "./Toggle";
+import { useState } from "react";
 
 function App() {
+  const [isOn, setIsOn] = useState(false);
+  const color = isOn ? "red" : "white";
   return (
     <div>
       <h3>Toggle</h3>
-      <Toggle />
+      <Toggle isOn={isOn} setIsOn= {setIsOn} color={color} />
     </div>
   );
 }
--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,12 @@
 import React from "react";
 
-function Toggle() {
-  return <button>OFF</button>;
+function Toggle({isOn, setIsOn, color}) {
+
+  function handleClick() {
+    setIsOn((isOn) => !isOn);
+  }
+
+  return <button style={{ background: color }} onClick={handleClick}>{isOn ? "ON" : "OFF"}</button>;
 }
 
 export default Toggle;
